bd3743df8c5059f1106e610567d6f2ea8eb682ec
kana-rus <kanarus786@gmail.com>
2023-01-30 00:01:19 +0900

    save@2023-01-30 00:501+9:00

--- src/main.rs
+++ src/main.rs
@@ -1,54 +1,80 @@
-use std::time::Duration;
+use std::{
+    io::{stdout, Stdout},
+    time::Duration,
+};
 use crossterm::{
-    terminal, Result,
+    Result,
+    cursor,
+    terminal, terminal::ClearType,
+    style, style::Print,
     event, event::{
-        Event, KeyCode, KeyEvent,
+        Event,
+        KeyCode,
         KeyModifiers as Modifier,
-    },
+    }, Command, ExecutableCommand,
 };
 
-#[derive(Debug, PartialEq)]
-struct Key {
-    code:     KeyCode,
-    modifier: Modifier,
-}
-impl From<KeyEvent> for Key {
-    fn from(value: KeyEvent) -> Self {
-        Self {
-            code:     value.code,
-            modifier: value.modifiers,
-        }
-    }
-}
 
 struct Config {
-    exit_key: Key,
+    exit_key: (Modifier, KeyCode),
 }
 impl Default for Config {
     fn default() -> Self {
         Self {
-            exit_key: Key {
-                code:     KeyCode::Char('q'),
-                modifier: Modifier::CONTROL,
-            },
+            exit_key: (Modifier::CONTROL, KeyCode::Char('q')),
         }
     }
 }
 
+struct Buffer {
+    stdout:  Stdout,
+    content: Vec<String>,
+}
+
+struct Window {
+    size: (u16, u16),
+}
+
+struct Cursor {
+    position: (u16, u16),
+}
+
 struct Editor {
     config: Config,
+    buffer: Buffer,
+    cursor: Cursor,
+    window: Window,
 }
 impl Editor {
     fn new() -> Result<Self> {
+        stdout()
+            .execute(terminal::Clear(ClearType::All))?
+            .execute(cursor::MoveTo(0, 0))?;
         terminal::enable_raw_mode()?;
         Ok(Self::default())
     }
-    fn read_input(&mut self) -> Result<()> {
-        while event::poll(Duration::from_secs(10))? {
+    fn execute(&mut self, command: impl Command) -> Result<()> {
+        self.buffer.stdout.execute(command)?;
+        Ok(())
+    }
+    fn edit(&mut self) -> Result<()> {
+        while event::poll(Duration::from_secs(60))? {
             if let Event::Key(e) = event::read()? {
-                let input = Key::from(e);
-                if input == self.config.exit_key {return Ok(())}
-                println!("{:?}\r", input)
+                let input = (e.modifiers, e.code);
+
+                if input == self.config.exit_key {break}
+                match input {
+                    (Modifier::NONE, KeyCode::Char(ch)) => self.execute(Print(ch))?,
+                    (_, KeyCode::Enter) => self.execute(Print("\r\n"))?,
+                    (_, KeyCode::Backspace | KeyCode::Delete) => self.execute(terminal::Clear(ClearType::CurrentLine))?,
+
+                    (_, KeyCode::Up   ) => self.execute(cursor::MoveUp(1))?,
+                    (_, KeyCode::Down ) => self.execute(cursor::MoveDown(1))?,
+                    (_, KeyCode::Left ) => self.execute(cursor::MoveLeft(1))?,
+                    (_, KeyCode::Right) => self.execute(cursor::MoveRight(1))?,
+
+                    _ => (),
+                }
             }
         }
         Ok(())
@@ -58,6 +84,16 @@ impl Default for Editor {
     fn default() -> Self {
         Self {
             config: Config::default(),
+            buffer: Buffer {
+                stdout:  stdout(),
+                content: vec![],
+            },
+            cursor: Cursor {
+                position: (0, 0),
+            },
+            window: Window {
+                size: terminal::size().unwrap(),
+            }
         }
     }
 }
@@ -68,6 +104,6 @@ impl Drop for Editor {
 }
 
 fn main() -> Result<()> {
-    Editor::new()?.read_input()?;
+    Editor::new()?.edit()?;
     Ok(())
 }
